a3884587c8c7714959a5f2931690e9526a6db2b4
Armen <armen.armen.armen95@gmail.com>
2020-09-04 07:17:40 +0300

    frozen

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,27 +4,54 @@
 //     }
 // }
 window.addEventListener('load', function() {
-    let $contentTitleWord = document.querySelector('.content__title-word');
+    let $contentWord = document.querySelector('.content__word'),
+        $contentTitle = document.querySelectorAll('.content__title');
+
+
+
+
+
 
     function autoPrintText(wordIndex, letterIndex, time) {
         setTimeout(function() {
-            if ($contentTitleWord.dataset.word.split(' ').length === wordIndex) {
+            if ($contentTitle[wordIndex] !== undefined) {
+                if ($contentTitle[wordIndex].dataset.text[letterIndex] !== undefined) {
+                    $contentTitle[wordIndex].textContent += $contentTitle[wordIndex].dataset.text[letterIndex];
+                    autoPrintText(wordIndex, ++letterIndex, 100);
+                } else {
+                    autoPrintText(++wordIndex, 0, 100);
+                }
+            } else {
+                $contentTitle[$contentTitle.length - 1].insertAdjacentElement('beforeend', $contentWord);
+                $contentWord.classList.add($contentWord.className + '--visibility');
+                autoPrintWord(0, 0, 200);
+            }
+        }, time);
+    }
+
+    function autoPrintWord(wordIndex, letterIndex, time) {
+        setTimeout(function() {
+            if ($contentWord.dataset.word.split(' ').length === wordIndex) {
                 wordIndex = 0;
                 letterIndex = 0;
             }
-            $contentTitleWord.innerHTML += $contentTitleWord.dataset.word.split(' ')[wordIndex][letterIndex];
-            if ($contentTitleWord.dataset.word.split(' ')[wordIndex].length - 1 === letterIndex) autoDeleteFunction(wordIndex, letterIndex, 800);
-            else autoPrintText(wordIndex, ++letterIndex, 350);
+            $contentWord.innerHTML += $contentWord.dataset.word.split(' ')[wordIndex][letterIndex];
+            if ($contentWord.dataset.word.split(' ')[wordIndex].length - 1 === letterIndex) autoDeleteFunction(wordIndex, letterIndex, 800);
+            else autoPrintWord(wordIndex, ++letterIndex, 350);
         }, time);
     }
 
     function autoDeleteFunction(wordIndex, letterIndex, time) {
         setTimeout(function() {
-            $contentTitleWord.innerHTML = $contentTitleWord.innerHTML.replace($contentTitleWord.dataset.word.split(' ')[wordIndex][letterIndex], '');
-            if ($contentTitleWord.innerHTML.length === 0) autoPrintText(++wordIndex, 0, 350);
+            $contentWord.innerHTML = $contentWord.innerHTML.replace($contentWord.dataset.word.split(' ')[wordIndex][letterIndex], '');
+            if ($contentWord.innerHTML.length === 0) autoPrintWord(++wordIndex, 0, 350);
             else autoDeleteFunction(wordIndex, --letterIndex, 350)
         }, time);
     }
 
-    autoPrintText(0, 0, 200);
+
+
+
+
+    if ($contentTitle.length) autoPrintText(0, 0, 100);
 });
